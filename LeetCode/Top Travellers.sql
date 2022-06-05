fab8c3384a45e02c6ceb2b09509ba9634d146558
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-06-05 23:43:56 +0900

    Create Top Travellers.sql

---
+++ LeetCode/Top Travellers.sql
@@ -0,0 +1,13 @@
+// sub-query
+select u.name, (select ifnull(sum(r.distance),0)
+                from rides r
+                where u.id = r.user_id) as travelled_distance
+from users u
+order by travelled_distance desc, name asc
+
+// left-join
+select u.name, ifnull(sum(r.distance),0) as travelled_distance
+from users u left outer join rides r
+on u.id = r.user_id
+group by u.name
+order by travelled_distance desc, name asc
