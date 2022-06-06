174f4be1fe1297ebc2292aabc35e34f5284e90ee
Bae12 <84948636+bae12-jo@users.noreply.github.com>
2022-06-06 23:47:14 +0900

    Update Top Travellers.sql

--- LeetCode/Top Travellers.sql
+++ LeetCode/Top Travellers.sql
@@ -1,11 +1,11 @@
-// sub-query
+// sub-query with ifnull
 select u.name, (select ifnull(sum(r.distance),0)
                 from rides r
                 where u.id = r.user_id) as travelled_distance
 from users u
 order by travelled_distance desc, name asc
 
-// left-join
+// left outer join with ifnull
 select u.name, ifnull(sum(r.distance),0) as travelled_distance
 from users u left outer join rides r
 on u.id = r.user_id
